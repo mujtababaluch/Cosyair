1518084826c9d5d316d93bd3d1ed8d22a41b8fc3
Mujtaba Ali <mujtabakalmati@gmail.com>
2022-03-06 08:57:21 -0800

    First Commit

---
+++ comapre.js
@@ -0,0 +1,169 @@
+ 
+function selectProduct1()
+        {
+
+            document.getElementById("product1").style.opacity = 1;
+
+            var product = document.getElementById("first").value;
+            if(product=="val1")
+            {
+                document.getElementById("image").src="images/chnag1.png";
+                document.getElementById("model").innerHTML="Model : DC Inverter Inventage 1.5 Ton";
+                document.getElementById("features").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+            else if(product=="val2")
+            {
+                document.getElementById("image").src="images/chang2.png" ;
+                document.getElementById("model").innerHTML="Model : 1.5 Ton Inverter Series HJ50VA";
+                document.getElementById("features").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+            else if(product=="val3")
+            {
+                document.getElementById("image").src="images/chang6.png" ;
+                document.getElementById("model").innerHTML="Model : CACS18SU3C3 1.5 Ton";
+                document.getElementById("features").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+            else if(product=="val4")
+            {
+                document.getElementById("image").src="images/cabinet-o2.png" ;
+                document.getElementById("model").innerHTML="Model : PS 3 2 Ton - Cabinet";
+                document.getElementById("features").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+            else if(product=="val5")
+            {
+                document.getElementById("image").src="images/cabinet-l3.png" ;
+                document.getElementById("model").innerHTML="Model : 4 Ton OFS - 48 S2";
+                document.getElementById("features").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+             else if(product=="val6")
+            {
+                document.getElementById("image").src="images/cabinet-m1.png" ;
+                document.getElementById("model").innerHTML="Model : TPC C 488 TICI 4 Ton - Cabinet";
+                document.getElementById("features").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+             else if(product=="val7")
+            {
+                document.getElementById("image").src="images/cassette-m2.png" ;
+                document.getElementById("model").innerHTML="Model : FDTC45KXE6F VRF Compact";
+                document.getElementById("features").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+             else if(product=="val8")
+            {
+                document.getElementById("image").src="images/cassette-o2.png" ;
+                document.getElementById("model").innerHTML="Model : HBU-36HH03";
+                document.getElementById("features").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+             else if(product=="val9")
+            {
+                document.getElementById("image").src="images/cassette-s2.png" ;
+                document.getElementById("model").innerHTML="Model : SN-96BA53 1 Ton";
+                document.getElementById("features").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+            else 
+            {
+                document.getElementById("product1").style.opacity = 0;
+            }
+        }
+
+ 
+function selectProduct2()
+        {
+
+            document.getElementById("product2").style.opacity = 1;
+
+            var product = document.getElementById("second").value;
+            if(product=="valu1")
+            {
+                document.getElementById("image2").src="images/chnag1.png" ;
+                document.getElementById("model2").innerHTML="Model : DC Inverter Inventage 1.5 Ton";
+                document.getElementById("features2").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec2").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+            else if(product=="valu2")
+            {
+                document.getElementById("image2").src="images/chang2.png" ;
+                document.getElementById("model2").innerHTML="Model : 1.5 Ton Inverter Series HJ50VA";
+                document.getElementById("features2").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec2").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+            else if(product=="valu3")
+            {
+                document.getElementById("image2").src="images/chang6.png" ;
+                document.getElementById("model2").innerHTML="Model : CACS18SU3C3 1.5 Ton";
+                document.getElementById("features2").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec2").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+            else if(product=="valu4")
+            {
+                document.getElementById("image2").src="images/cabinet-o2.png" ;
+                document.getElementById("model2").innerHTML="Model : PS 3 2 Ton - Cabinet";
+                document.getElementById("features2").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec2").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+            else if(product=="valu5")
+            {
+                document.getElementById("image2").src="images/cabinet-l2.png" ;
+                document.getElementById("model2").innerHTML="Model : 4 Ton OFS - 48 S2";
+                document.getElementById("features2").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec2").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+             else if(product=="valu6")
+            {
+                document.getElementById("image2").src="images/cabinet-m2.png" ;
+                document.getElementById("model2").innerHTML="Model : TPC C 488 TICI 4 Ton - Cabinet";
+                document.getElementById("features2").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec2").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+             else if(product=="valu7")
+            {
+                document.getElementById("image2").src="images/cassette-m2.png" ;
+                document.getElementById("model2").innerHTML="Model : FDTC45KXE6F VRF Compact";
+                document.getElementById("features2").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec2").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+             else if(product=="valu8")
+            {
+                document.getElementById("image2").src="images/cassette-o2.png" ;
+                document.getElementById("model2").innerHTML="Model : HBU-36HH03";
+                document.getElementById("features2").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec2").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+             else if(product=="valu9")
+            {
+                document.getElementById("image2").src="images/cassette-s2.png" ;
+                document.getElementById("model2").innerHTML="Model : SN-96BA53 1 Ton";
+                document.getElementById("features2").innerHTML="Features: Air Vane Technology | Auto Swing | Power Mode<br>";
+                document.getElementById("spec2").innerHTML="Specifications:  Cooling (Btu/H) : 53000 | Power Input (W) : 4800 | EER : 5.5-3.2<br>";
+                
+            }
+            else 
+            {
+                document.getElementById("product1").style.opacity = 0;
+            }
+        }
